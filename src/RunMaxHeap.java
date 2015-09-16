3cfed89a225f3e9be52a0625d0f3f84410844f09
Andrew Petriccione <apetricc@unca.edu>
2015-09-16 18:33:12 -0400

    After tutor Wednesday. Heapsort seems to be working, still need more comments etc, & maybe some more rigorous testing

--- src/RunMaxHeap.java
+++ src/RunMaxHeap.java
@@ -5,12 +5,13 @@ public class RunMaxHeap {
 
 
     public static void main(String[] args) {
-        int[] myArr = {1, 2, 8, 4, 5, 6, 7, 3};
+        int[] myArr = {1, 22, 8, 4, 15, 6, 7, 99};
         MaxHeap myMaxHeap = new MaxHeap(myArr);
         myMaxHeap.heapsort();
-        System.out.println(myMaxHeap.printMaxHeap(myArr));
+        System.out.println(myMaxHeap.printMaxHeap());
 
         for (int i = 0; i < myArr.length - 1; i++) {
+            System.out.println("The parent of " + i + " is:" + myMaxHeap.parentOf(i));
             System.out.println("The left child of index " +i +" is: " + myMaxHeap.leftChildOf(i));
             System.out.println("The right child of index " +i  +" is: " + myMaxHeap.rightChildOf(i));
         }
